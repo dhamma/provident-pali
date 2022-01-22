074072f8bec169f9335a40217a92f78cffd9d263
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2022-01-22 08:39:22 +0630

    fix fromDevanagari

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 ﻿import {fromIAST,toIAST} from "./src/iast.js"
 export * from "./src/ipa.js"
-import {toIndic} from "./src/indic.js"
+import {toIndic,fromDevanagari} from "./src/indic.js"
 export * from "./src/utils.js"
 export const provident2indic=(str,script='')=>{
     if (!script) return str;
     if (script=='iast') return toIAST(str);
     else return toIndic(str,script)
 }
-export {fromIAST,toIAST};
+export {fromIAST,toIAST,fromDevanagari};
--- src/indic.js
+++ src/indic.js
@@ -60,7 +60,7 @@ export const fromDevanagari=content=>{
             }
         }
     });
-    // out=out.replace(/\u200d/g,'');
+    out=out.replace(/\u200d/g,'');
     out=out.replace(/[ऀ-ॿ]/g,''); //drop all unknown
     return out;
 }
